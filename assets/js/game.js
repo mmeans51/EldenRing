254d0d3b13f2009f228774b2b27cf7d9c8a57cb7
michael Means <colemeans51@gmail.com>
2022-08-22 19:10:53 -0500

    Update game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,17 @@
+var playerName = window.prompt("what is ye name tarnished?;")
+var playerHealth = 100;
+var playerAttack = 10;
+
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Morgot";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
 function fight() {
-    window.alert("this fight has begun");
+    window.alert("AAAGH... RISE NOW, YE TARNISHED! YE DEAD, WHO YET LIVE!");
 }
 
-fight();
+console.log("our robot's name is " + playerName);
+
+// fight();
