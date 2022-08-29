15d4de6324e60fc3e0d21134d6475cfc397bdf46
michael Means <colemeans51@gmail.com>
2022-08-29 15:23:34 -0500

    Update game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,57 +3,58 @@ var playerAttack = 10;
 var playerMoney = 1500;
 
 var enemyName = [
-    "Margit, the Fell Omen",
-    "Starscourge Radahn",
-    "Dragonlord Placidusax",
+  "Margit, the Fell Omen",
+  "Starscourge Radahn",
+  "Dragonlord Placidusax",
 ];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-
 var playerName = window.prompt("what is ye name tarnished?;");
 window.alert("AAAGH... RISE NOW, YE TARNISHED! YE DEAD, WHO YET LIVE!");
 
-
 var fight = function (enemyName) {
-  while(enemyHealth) {
   var promptFight = window.prompt("Does the tarnish wish to flee or fight?");
   if (promptFight === "fight" || promptFight === "FIGHT") {
-    enemyHealth = enemyHealth - playerAttack;
-    console.log(
-      playerName +
-        " attacked " +
-        enemyName +
-        ". " +
-        enemyName +
-        " now has " +
-        enemyHealth +
-        " health remaining."
-    );
-    window.alert(playerName + " hit " + enemyName + ".     -" + playerAttack);
-    if (enemyHealth <= 0) {
-      window.alert("GREAT ENEMY FELLED");
-    } else {
-      window.alert(enemyName + "  has " + enemyHealth + "HP left");
-    }
-
-    playerHealth = playerHealth - enemyAttack;
-    window.alert(enemyName + " hits " + playerName + ".     -" + enemyAttack);
-    console.log(
-      enemyName +
-        " attacked " +
+    while (playerHealth > 0 && enemyHealth > 0) {
+      enemyHealth = enemyHealth - playerAttack;
+      console.log(
         playerName +
-        ". " +
-        playerName +
-        " now has " +
-        playerHealth +
-        "HP remaining."
-    );
+          " attacked " +
+          enemyName +
+          ". " +
+          enemyName +
+          " now has " +
+          enemyHealth +
+          " health remaining."
+      );
+      window.alert(playerName + " hit " + enemyName + ".     -" + playerAttack);
+      if (enemyHealth <= 0) {
+        window.alert("GREAT ENEMY FELLED");
+        break;
+      } else {
+        window.alert(enemyName + "  has " + enemyHealth + "HP left");
+      }
 
-    if (playerHealth <= 0) {
-      window.alert("YOU DIED");
-    } else {
-      window.alert(playerName + "  has " + playerHealth + " HP left");
+      playerHealth = playerHealth - enemyAttack;
+      window.alert(enemyName + " hits " + playerName + ".     -" + enemyAttack);
+      console.log(
+        enemyName +
+          " attacked " +
+          playerName +
+          ". " +
+          playerName +
+          " now has " +
+          playerHealth +
+          "HP remaining."
+      );
+
+      if (playerHealth <= 0) {
+        window.alert("YOU DIED");
+        break;
+      } else {
+        window.alert(playerName + "  has " + playerHealth + " HP left");
+      }
     }
   } else if (promptFight === "flee" || promptFight === "FLEE") {
     var confirmSkip = window.confirm(
@@ -62,15 +63,17 @@ var fight = function (enemyName) {
     if (confirmSkip) {
       window.alert(playerName + "has has fled.     -200");
       playerMoney = playerMoney - 200;
+      console.log("playerMoney", playerMoney);
     } else {
       fight();
     }
   } else {
     window.alert("Tarnished must FIGHT or FLEE!");
   }
-}
 };
 
-for(var i = 0; i < enemyName.length; i++) {
-    fight(enemyName[i]);
+for (var i = 0; i < enemyName.length; i++) {
+  var pickedEnemyName = enemyName[i];
+  enemyHealth = 50;
+  fight(pickedEnemyName);
 }
